d5899abdd48c41ab72a1ccf3174461f4d73ca5fa
Maurits van Rees <maurits@vanrees.org>
2022-05-06 13:16:16 +0200

    Install the wheel package in CI.
    I see messages like this on GHA:
    Using legacy 'setup.py install' for Chameleon, since package 'wheel' is not installed.

--- prep.sh
+++ prep.sh
@@ -3,7 +3,7 @@
 # then it will take the correct Python version.
 BASEDIR=$(dirname $(realpath $0))
 python -m pip install --upgrade pip
-pip install mxdev libvcs==0.11.1
+pip install wheel mxdev libvcs==0.11.1
 mxdev -c sources.ini
 pip install -r${BASEDIR}/requirements-mxdev.txt
 python -V
