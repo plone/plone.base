5babbfb66ee099ae62c119dec4fea96c0c6375eb
Maurits van Rees <maurits@vanrees.org>
2022-05-06 12:56:26 +0200

    Add CI for plone.base.
    We should catch it when we miss a dependency, or notice when a dependency pulls in too many others.
    We currently miss the `plone.schema` dependency, needed for the `Email` field.
    If we add it, it pulls in `plone.app.z3cform` for `IPloneFormLayer`, and this package pulls in lots of `plone.app` packages.

---
+++ prep.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This script works best when run in GitHub Actions or using tox:
+# then it will take the correct Python version.
+BASEDIR=$(dirname $(realpath $0))
+python -m pip install --upgrade pip
+pip install mxdev libvcs==0.11.1
+mxdev -c sources.ini
+pip install -r${BASEDIR}/requirements-mxdev.txt
+python -V
+pip list
