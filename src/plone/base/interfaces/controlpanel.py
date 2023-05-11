3bcb77de35119208418c30d68813e0510d8257cf
Eric Wohnlich <eric.wohnlich@gmail.com>
2023-05-11 17:45:21 -0400

    Add help plugin as an option

--- src/plone/base/interfaces/controlpanel.py
+++ src/plone/base/interfaces/controlpanel.py
@@ -543,6 +543,7 @@ class ITinyMCEPluginSchema(Interface):
                     SimpleTerm("emoticons", "emoticons", "emoticons"),
                     SimpleTerm("fullpage", "fullpage", "fullpage"),
                     SimpleTerm("fullscreen", "fullscreen", "fullscreen"),
+                    SimpleTerm("help", "help", "help"),
                     SimpleTerm("hr", "hr", "hr"),
                     SimpleTerm("insertdatetime", "insertdatetime", "insertdatetime"),
                     SimpleTerm("layer", "layer", "layer"),
