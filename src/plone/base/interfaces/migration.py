86a48683d60b972a2f895fac63519bbd4f63d0c4
Maurits van Rees <maurits@vanrees.org>
2026-03-24 16:19:13 +0100

    Add `interfaces.migration.IAddonList`.
    This is for https://github.com/plone/Products.CMFPlone/pull/4295
    Currently that CMFPlone PR contains the interface that I now move here.

--- src/plone/base/interfaces/migration.py
+++ src/plone/base/interfaces/migration.py
@@ -1,4 +1,29 @@
+from zope.interface import Attribute
 from zope.interface import Interface
+
+
+class IAddonList(Interface):
+    """Utility providing a list of add-ons managed by the migration tool.
+
+    * addon_list is the list of add-ons that are upgraded at the end
+      of the migration
+    * pre_addon_list is the list of add-ons that are upgraded at the start
+      of the migration.
+
+    The pre_addon_list is optional.  If you have a Plone distribution with an
+    own base profile, you may want to add the default Plone profile here,
+    so the core of Plone is updated first:
+
+    pre_addon_list = AddonList([Addon(profile_id="Products.CMFPlone:plone")])
+
+    Maybe add a part of the standard ADDON_LIST from
+    Products.CMFPlone.MigrationTool as well.
+    But if you find yourself adding *all* of them, then your distribution
+    probably doesn't need its own base profile and add-on list.
+    """
+
+    addon_list = Attribute("add-ons upgraded at end of migration.")
+    pre_addon_list = Attribute("add-ons upgraded at start of migration.")
 
 
 class IMigrationTool(Interface):
